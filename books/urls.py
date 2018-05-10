b3e03a78c173f982d84461a861325588ce21f234
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-10 22:44:41 +0800

    完整地实现了一个简单的搜索`表单`的流程。 从创建一个`搜索表单`，到最终把提交的搜索内容，显示出来；确保`表单处理流程`是畅通的。 1.本次补全了之前所遗漏的`视图函数`：`search()`; 2.利用`request.GET['q']`，获取了搜索提交的内容。还做了安全检查，并将其输入的搜索内容`显示`出来，从而确认了`表单处理流程`是通畅的。

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,5 @@ from books import views
 
 urlpatterns = [
     url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 ]
--- books/views.py
+++ books/views.py
@@ -5,7 +5,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def search_form(request):
     return render(request, 'books/search_form.html')
+
+def search(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
