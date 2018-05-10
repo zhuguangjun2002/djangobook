c833e791b8da661255981509196b187a41aa158a
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-10 20:26:40 +0800

    在处理`Book`模型，字段`authors`在`编辑窗体`中进行多项选择时，推荐使用`filter_horizontal`。 该控件对用户使用任何`ManyToManyField`字段都非常便捷。

--- books/admin.py
+++ books/admin.py
@@ -16,7 +16,7 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)
-    fields = ('title', 'authors', 'publisher')
+    filter_horizontal = ('authors',)
 
 
 admin.site.register(Publisher)
