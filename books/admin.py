db07a822dbdccc44fc666c1b340bf236698dd925
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-10 17:05:51 +0800

    对`Auther`类在`admin site`上进行`变更列表`页面，进行`定制`。 确定`显示列表`，包括：`'first_name', 'last_name', 'email'`。

--- books/admin.py
+++ books/admin.py
@@ -7,8 +7,10 @@ from django.contrib import admin
 
 from .models import Publisher, Author, Book
 
-import sys;
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
 
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author,AuthorAdmin)
 admin.site.register(Book)
