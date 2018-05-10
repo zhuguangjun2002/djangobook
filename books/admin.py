4cd6cc98caf2c033b0c838dfe5e13d2694f1d516
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-10 17:17:48 +0800

    在`管理界面`，为`Author`模型的`列表`，添加`搜索框`。 具体搜索的字段为： search_fields = ('first_name', 'last_name')

--- books/admin.py
+++ books/admin.py
@@ -9,6 +9,7 @@ from .models import Publisher, Author, Book
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
 
 
 admin.site.register(Publisher)
