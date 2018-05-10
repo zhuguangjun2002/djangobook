bb072e11f333f8cace7810132d4254685f6bc998
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-10 18:33:34 +0800

    通过在`BookAdmin`类中，添加`ordering`字段，实现在`管理界面`的`Book`模型的`排序`，按照`publication_date`（出版日期）的`倒序`进行显示。 默认情况下，`Book`模型在`管理界面`中的`更改列表`，会根据`Book`模型内部的`class Meta`中的`ordering`的值，对其`对象`进行排序。 但是，因为我们还没有指定此`ordering`的值，则`排序`未定义。 所以，为了添加该功能，我们在类`BookAdmin`中，添加了排序字段：` ordering = ('-publication_date',)`，从而确保`排序`是安装`出版日期`的倒序显示的。

--- books/admin.py
+++ books/admin.py
@@ -15,6 +15,8 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
+
 
 admin.site.register(Publisher)
 admin.site.register(Author,AuthorAdmin)
