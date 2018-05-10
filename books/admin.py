210ca6613a5a8a66051e0c285812483efe2c1aa7
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-10 19:39:47 +0800

    在我们的`图书`编辑页面，我们可以隐藏`publication_date`字段，让其不可以被`编辑`： 字段`fields`中，删除'publication_date'，其他的保留:  fields = ('title', 'authors', 'publisher')

--- books/admin.py
+++ books/admin.py
@@ -16,7 +16,7 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)
-    fields = ('title', 'authors', 'publisher', 'publication_date')
+    fields = ('title', 'authors', 'publisher')
 
 
 admin.site.register(Publisher)
