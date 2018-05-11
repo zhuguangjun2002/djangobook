54a9cf652d11d4b21a5ea3b9ff630ca58977f0a6
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-11 14:21:45 +0800

    直接在`books/view.py`中,实现函数`search()`的功能。可以正常执行。 使用`html`变量，构造了一个返回结果，包含了`标题`(`title`)和`出版社`(`publisher`)。 这是一个快速方法:`A quick and dirty way`,接下来会使用`模板`进行优化。

--- books/views.py
+++ books/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Book
 
 
 def search_form(request):
@@ -13,7 +14,15 @@ def search_form(request):
 
 def search(request):
     if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        html = []
+        for book in books:
+            title = book.title
+            publisher = book.publisher
+            html.append('<tr><td>{0}</td><td>{1}</td></tr>'.format(title,publisher))
+        #html = '<table>{0}</table>'.format('\n'.join(html))
+        html = '<html><body><table>{0}</table></body></html>'.format('\n'.join(html))
+        return HttpResponse(html)
     else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        return HttpResponse('You submitted an empty form.')
