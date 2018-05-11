d8c82125c84e7f92e232a9fe98ef4a9faf89def9
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-11 18:35:04 +0800

    改进了提交`搜索字符串`为`空`的情况。 当提交的搜索字符串为空时，`用户`不用再按下`后退按钮`返回到搜索界面。 我们把这一情况，直接发送给模板`search_form.html`,传递给它一个`error`变量， 让其根据是否存在这个`error`变量，决定是否显示错误信息。 默认情况下，因为没有`error`变量，所以，渲染`search_form.html`时，只是显示表单，不会显示错误信息。

--- books/views.py
+++ books/views.py
@@ -13,10 +13,13 @@ def search_form(request):
     return render(request, 'books/search_form.html')
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
+    error= False
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render(request, 'books/search_results.html',
-                      {'books': books, 'query': q})
-    else:
-        return HttpResponse('Please submit a search term.')
+        if not q :
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'books/search_results.html',
+                          {'books': books, 'query': q})
+    return render(request,'books/search_form.html',{'error':error})
