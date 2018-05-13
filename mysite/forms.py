dbc8810ceaac44907445ef04a1eadb48bebb745e
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-13 11:28:24 +0800

    修改`ContactForm`，让其对`message`字段，必须大于`4个`词。 通过使用`自定义`的`方法`：`clean_message`，实现该功能。

--- mysite/forms.py
+++ mysite/forms.py
@@ -4,3 +4,10 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     email = forms.EmailField(required=False)
     message = forms.CharField(widget=forms.Textarea)
+
+    def clean_message(self):
+        message = self.cleaned_data['message']
+        num_words = len(message.split())
+        if num_words < 4:
+            raise forms.ValidationError("Not engough words!")
+        return message
