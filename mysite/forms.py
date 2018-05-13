d90a4719bb8a192223d8ed3a5378864e7f32164f
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-13 16:02:34 +0800

    为`ContactForm`的`email`字段，添加定制化的`label`(`标签`)。

--- mysite/forms.py
+++ mysite/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=False, label='Your e-mail address')
     message = forms.CharField(widget=forms.Textarea)
 
     def clean_message(self):
