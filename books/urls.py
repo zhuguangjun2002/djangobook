791b2c75e858c183d9fee3407c1c9aeafbf6acb0
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-15 11:54:16 +0800

    添加了`PublisherDetail`,但是始终无法访问页面：`http://127.0.0.1:8000/details/2/`； 总提示：`没有找到符合查询的`出版商``。

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from books import views
-from books.views import PublisherList
+from books.views import PublisherList,PublisherDetail
 
 urlpatterns = [
     url(r'^search/$', views.search),
     url(r'^publishers/$',PublisherList.as_view()),
+    # url(r'^details/(?P<pk>[0-9]+)/$',PublisherDetail.as_view(), name='publisher-detail'),
+    # url(r'^details/(?P<pk>[0-9]+)/$', PublisherDetail.as_view(), name='publisher-detail'),
+    url(r'^details/(?P<pk>[0-9]+)/$', PublisherDetail.as_view()),
 ]
--- books/views.py
+++ books/views.py
@@ -12,6 +12,9 @@ from .models import Book
 from django.views.generic import ListView
 from books.models import Publisher
 
+# define a detailview
+from django.views.generic import DetailView
+
 def search(request):
     errors= []
     if 'q' in request.GET:
@@ -29,3 +32,15 @@ def search(request):
 class PublisherList(ListView):
     model = Publisher
     context_object_name = 'my_favorite_publishers'
+
+class PublisherDetail(DetailView):
+    model = Publisher
+    # pk_url_kward = 'id'
+    # tempalte_name = 'books/publisher_detail.html'
+
+    # def get_context_data(self, **kwargs):
+        # # Call the base implementaion first to get a context
+        # context = super(PublisherDetail,self).get_context_data(**kwargs)
+        # # Add in a QuerySet of all the books
+        # #context['book_list'] = Book.objects.all()
+        # return context
