4659449297cf403e90cd0b2dcb3d889027c519f3
zhuguangjun2002 <zhuguangjun2002@163.com>
2018-05-19 10:26:51 +0800

    参考`芬兰男孩`的《How to Add User Profile To Django Admin》。 该文章是基于采用《How to Extend Django User Model》中策略2：` Using One-To-One Link With a User Model (Profile）`。 在其基础之上，实现了在`后台管理平台admin`上，显示`用户`的`Profile`的功能。 里面还用到`数据库查询优化`的技术，详见《Django Tips #3 Optimize Database Queries》。 主要是使用字段: `list_select_related = ('profile', )`,否则效率会很低。

--- core/models.py
+++ core/models.py
@@ -10,16 +10,29 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
+    STUDENT = 1
+    TEACHER = 2
+    SUPERVISOR = 3
+    ROLE_CHOICE = (
+            (STUDENT,'Student'),
+            (TEACHER,'Teacher'),
+            (SUPERVISOR,'Supervisor'),
+            )
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     bio = models.TextField(max_length=500,blank=True)
     location = models.CharField(max_length=30,blank=True)
     birth_date = models.DateField(null=True,blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE,null=True,blank=True)
+
+    def __unicode__(self): # __unicode__ for Python 2
+        return self.user.username
 
 @receiver(post_save,sender=User)
-def create_user_profile(sender,instance,created,**kwargs):
+def create_or_update_user_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-@receiver(post_save,sender=User)
-def save_user_profile(sender,instance,**kwargs):
     instance.profile.save()
+
+# @receiver(post_save,sender=User)
+# def save_user_profile(sender,instance,**kwargs):
+    # instance.profile.save()
